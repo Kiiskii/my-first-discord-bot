f2069c13d8f9c600097bd9a452a9fddbe7b2d060
Anton <antonoliverkiiski@gmail.com>
2022-12-13 10:34:29 +0200

    Remove token from code

--- my-first-discord-bot/Program.cs
+++ my-first-discord-bot/Program.cs
@@ -14,7 +14,7 @@ namespace my_first_discord_bot
         {
             var discord = new DiscordClient(new DiscordConfiguration()
             {
-                Token = "MTA1MDMzMjk3ODI5MjcyMzc2Mw.GPpiKU.8qwG1DDnn229ll6MClQVdN0FkRzEl7JI163yOE",
+                Token = "",
                 TokenType = TokenType.Bot,
                 Intents = DiscordIntents.AllUnprivileged
             });
